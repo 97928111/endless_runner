f854333ac319740b394897e06f6a15b3fb9bf4f1
Jimmy <jimmylu10@gmail.com>
2022-04-23 01:41:52 -0700

    new

---
+++ src/main.js
@@ -0,0 +1,34 @@
+let game;
+ 
+// global game options
+let gameOptions = {
+    platformStartSpeed: 350,
+    spawnRange: [100, 350],
+    platformSizeRange: [50, 250],
+    playerGravity: 900,
+    jumpForce: 400,
+    playerStartPosition: 200,
+    jumps: 2
+}
+ 
+window.onload = function() {
+ 
+    // object containing configuration options
+    let gameConfig = {
+        type: Phaser.AUTO,
+        width: 1334,
+        height: 750,
+        scene: [Play],
+        backgroundColor: 0x444444,
+ 
+        // physics settings
+        physics: {
+            default: "arcade"
+        }
+    }
+    game = new Phaser.Game(gameConfig);
+    window.focus();
+    resize();
+    window.addEventListener("resize", resize, false);
+}
+
